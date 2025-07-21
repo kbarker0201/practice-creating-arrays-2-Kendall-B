8a77969378cb7eb587073d910ff20aac79ae3e31
kbarker0201 <kbarker0201@gmail.com>
2025-07-20 19:50:37 -0500

    final changes

--- practice.js
+++ practice.js
@@ -1,1 +1,15 @@
-console.log("hi");
+// Task 1
+let newArr = Array(5);
+newArr.fill("Hello");
+console.log(newArr);
+
+// Task 2 
+newArr.fill("What!", 2, 4);
+console.log(newArr);
+
+// Task 3 
+let secondArr = Array(5);
+for (let i = 0; i < secondArr.length; i++) {
+ secondArr[i] = i * 10; // Fill each slot with i2
+}
+console.log(secondArr);
